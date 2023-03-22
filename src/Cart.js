513b71c7818a62dda9816366d61b738e4ac5849f
Ryan Fuhrman <rsfuhrman10@gmail.com>
2023-03-22 11:39:14 -0400

    Sort items on cart page by ID

--- src/Cart.js
+++ src/Cart.js
@@ -3,32 +3,55 @@ import React, { useState, useEffect } from "react";
 function Cart({ cartItems }) {
   const [cartList, setCartList] = useState([]);
 
-  useEffect(() => {
-    console.log(cartItems);
+  // if two items in cart have same id, combine them
+  function fetchItems() {
     cartItems.map((item) => {
       const fetchData = async () => {
         const response = await fetch(
           `https://api.escuelajs.co/api/v1/products/${item.id}`
         );
         const data = await response.json();
-        setCartList((prevList) => [...prevList, data]);
+        setCartList((prevList) => [
+          ...prevList,
+          { data, count: Number(item.count) },
+        ]);
       };
 
       fetchData();
     });
+  }
+
+  useEffect(() => {
+    fetchItems();
   }, []);
 
   useEffect(() => {
+    const sortedList = cartList.sort((a, b) => {
+      return a.data.id - b.data.id;
+    });
+    setCartList(sortedList);
     console.log(cartList);
   }, [cartList]);
+
+  function updateCount() {}
 
   return (
     <div>
       <ul>
-        {cartList.map((item) => {
+        {cartList.map(({ data, count }) => {
           return (
-            <li key={item.id} id={item.id}>
-              {item.id}
+            <li key={data.id} id={data.id}>
+              <img src={data.images[0]} />
+              <h4>{data.title}</h4>
+              <p>{count}</p>
+              <form className="buttons">
+                <input
+                  type="number"
+                  onChange={updateCount}
+                  value={count}
+                ></input>
+                <button>Remove Item</button>
+              </form>
             </li>
           );
         })}
